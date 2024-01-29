26f54939f8bc489482f86d934faede8996d26707
Cody <codylau6233@hotmail.com>
2024-01-29 13:38:42 -0800

    remove duplicated cookieSession.

--- server.js
+++ server.js
@@ -5,19 +5,11 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
-const cookieSession = require('cookie-session');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(cookieSession({
-  name: 'session',
-  keys: ['testkey1'],
-
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -40,6 +32,13 @@ const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['testkey1'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
