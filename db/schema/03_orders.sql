fcf04ac180ccad201c38ffd5bb1ad9e2f3f4fe3d
Phil <philippe.teigne@gmail.com>
2024-01-30 22:20:37 -0500

    add placed_at column to orders table to track order status

--- db/schema/03_orders.sql
+++ db/schema/03_orders.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INT REFERENCES users(id) ON DELETE CASCADE,
-  created_at TIMESTAMP,
-  time_to_complete INT
+  created_at DATETIME DEFAULT NOW(),
+  placed_at DATETIME DEFAULT NULL,
+  time_to_complete INT DEFAULT NULL
 );
