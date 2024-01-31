2d89b24d15af72fb51df89d493e5b67f2b81090a
Phil <philippe.teigne@gmail.com>
2024-01-30 22:15:11 -0500

    moved routes from server.js to routes/users

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,48 @@ router.get('/', (req, res) => {
   res.render('users');
 });
 
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+// ORDER HISTORY
+router.get('/orders/:userID', (req, res) => {
+  //  Check if the user is logged in
+  //  Populate from SQL orders for userID
+  res.render('orders');
+});
+
+// VIEW CART
+router.get('/orders/:orderID', (req, res) => {
+  // Check if the user is logged in
+  // Populate currentOrder object from cookie data
+  // 
+  res.render('cart', currentOrder);
+});
+
+// USER LOGIN
+router.get('/login/:id', (req, res) => {
+  // using encrypted cookies
+  req.session.user_id = req.params.id;
+
+  // or using plain-text cookies
+  res.cookie('user_id', req.params.id);
+
+  // send the user somewhere
+  res.redirect('/');
+});
+
+router.post('/order/:orderID', (req, res) => {
+  // create sql statement to add order to orders table
+  // make api request to send notification to restaurant
+  res.redirect('orders/:userID');
+});
+
+router.post('/orders/:orderID/timeToComplete', (req, res) => {
+  // create sql statement for owner to add/update time_to_complete to orders table.
+  // send sms to client to notify their order is being prepared
+  // start a timer that sends an sms to the client when completed
+  res.redirect('orders/:userID');
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -54,52 +54,6 @@ app.use('/users', usersRoutes);
 
 // when user adds to cart store the information in the cookie
 
-// HOME PAGE
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-// ORDER HISTORY
-app.get('/orders/:userID', (req, res) => {
-  //  Check if the user is logged in
-  //  Populate from SQL orders for userID
-  res.render('orders');
-});
-
-// VIEW CART
-app.get('/orders/:orderID', (req, res) => {
-  // Check if the user is logged in
-  // Populate currentOrder object from cookie data
-  // 
-  res.render('cart', currentOrder);
-});
-
-// USER LOGIN
-app.get('/login/:id', (req, res) => {
-  // using encrypted cookies
-  req.session.user_id = req.params.id;
-
-  // or using plain-text cookies
-  res.cookie('user_id', req.params.id);
-
-  // send the user somewhere
-  res.redirect('/');
-});
-
-app.post('/order/:orderID', (req, res) => {
-  // create sql statement to add order to orders table
-  // make api request to send notification to restaurant
-  res.redirect('orders/:userID');
-});
-
-app.post('/orders/:orderID/timeToComplete', (req, res) => {
-  // create sql statement for owner to add/update time_to_complete to orders table.
-  // send sms to client to notify their order is being prepared
-  // start a timer that sends an sms to the client when completed
-  res.redirect('orders/:userID');
-});
-
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
