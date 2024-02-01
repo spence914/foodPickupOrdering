ba1c5e880a38829f90baad0fefb7dd12cc227df4
Spencer Lewis <spence.914@gmail.com>
2024-02-01 14:11:19 -0800

    add seeds to db so that all users have orders populated with items

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -5,3 +5,4 @@ INSERT INTO orders (user_id, created_at) VALUES (2, '2024-01-29 11:15:00');
 INSERT INTO orders (user_id, created_at) VALUES (2, '2024-01-29 12:30:00');
 INSERT INTO orders (user_id, created_at) VALUES (4, '2024-01-29 13:45:00');
 INSERT INTO orders (user_id, created_at) VALUES (5, '2024-01-29 14:00:00');
+INSERT INTO orders (user_id, created_at) VALUES (5, '2024-02-01 14:00:00');
--- db/seeds/04_order_contents.sql
+++ db/seeds/04_order_contents.sql
@@ -5,3 +5,12 @@ INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (1, 2, 1);
 INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (2, 3, 3);
 INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (2, 4, 1);
 INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (3, 5, 2);
+
+INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (4, 1, 1);
+INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (4, 2, 2);
+
+INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (5, 3, 1);
+INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (5, 4, 1);
+
+INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (6, 5, 2);
+INSERT INTO order_contents (order_id, food_item_id, quantity) VALUES (6, 1, 1);
