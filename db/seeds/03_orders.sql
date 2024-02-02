b5ff69f55bc6ac54d36d29a28da42ae28db53805
Spencer Lewis <spence.914@gmail.com>
2024-02-01 20:16:18 -0800

    change status for seeded orders in db

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -1,8 +1,8 @@
  -- orders table seeds:
 
-INSERT INTO orders (user_id, created_at) VALUES (1, '2024-01-29 10:00:00');
-INSERT INTO orders (user_id, created_at) VALUES (2, '2024-01-29 11:15:00');
-INSERT INTO orders (user_id, created_at) VALUES (2, '2024-01-29 12:30:00');
-INSERT INTO orders (user_id, created_at) VALUES (4, '2024-01-29 13:45:00');
-INSERT INTO orders (user_id, created_at) VALUES (5, '2024-01-29 14:00:00');
-INSERT INTO orders (user_id, created_at) VALUES (5, '2024-02-01 14:00:00');
+INSERT INTO orders (user_id, created_at, placed_at, status) VALUES (1, '2024-01-29 10:00:00', '2024-01-29 10:05:00', 'open');
+INSERT INTO orders (user_id, created_at, placed_at, status) VALUES (2, '2024-01-29 11:15:00', '2024-01-29 11:20:00', 'open');
+INSERT INTO orders (user_id, created_at, placed_at, status) VALUES (2, '2024-01-29 12:30:00', '2024-01-29 12:35:00', 'open');
+INSERT INTO orders (user_id, created_at, placed_at, status) VALUES (4, '2024-01-29 13:45:00', '2024-01-29 13:50:00', 'open');
+INSERT INTO orders (user_id, created_at, placed_at, status) VALUES (5, '2024-01-29 14:00:00', '2024-01-29 14:05:00', 'open');
+INSERT INTO orders (user_id, created_at, placed_at, status) VALUES (5, '2024-02-01 14:00:00', '2024-02-01 14:05:00', 'open');
