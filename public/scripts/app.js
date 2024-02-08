b3c0d067d8b3abbece88c4904bf40350c1394599
Phil <philippe.teigne@gmail.com>
2024-02-08 18:12:30 -0500

    add ajax scripts to index page and remove button type submits

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,69 @@
-// Client facing scripts here
-$('.quantityAjax').on("keyup keydown change", function() {
-  $(".update-form").trigger("submit");
-  $("#subtotalTarget").load("/cart #subtotalTarget");
-});
-
-$('.update-form').submit(function(e) {
-  e.preventDefault();
-  const postUrl = $(this).attr("action");
-  const values = $(this).serialize();
-
-  $.ajax({
-    type: "POST",
-    url: postUrl,
-    data: values,
-    success: (res) => {
-      console.log("success");
-    }
+$(document).ready(function() {
+  
+  // Client facing scripts here
+  $('.quantityAjax').on("keyup keydown change", function() {
+    $(".update-form").trigger("submit");
+    $("#subtotalTarget").load("/cart #subtotalTarget");
   });
-});
 
-$('.remove-btn').on('click', function() {
-  $(this).closest(".removeItemAjax").trigger("submit");
-  const $foodItem = $(this).closest('.foodItem');
-  $foodItem.hide();
-  $("#subtotalTarget").load("/cart #subtotalTarget");
-});
+  $('.update-form').submit(function(e) {
+    e.preventDefault();
+    const postUrl = $(this).attr("action");
+    const values = $(this).serialize();
+    
+    $.ajax({
+      type: "POST",
+      url: postUrl,
+      data: values,
+      success: (res) => {
+        console.log("success");
+      }
+    });
+    
+  });
 
-// Remove foodItem
-$('.removeItemAjax').submit(function(e) {
-  e.preventDefault();
-  const postURL = $(this).attr("action");
-  const values = $(this).serialize();
+  $('.remove-btn').on('click', function() {
+    $(this).closest(".removeItemAjax").trigger("submit");
+    const $foodItem = $(this).closest('.foodItem');
+    $foodItem.hide();
+    $("#subtotalTarget").load("/cart #subtotalTarget");
+  });
 
-  $.ajax({
-    type: "POST",
-    url: postURL,
-    data: values,
-    success: (res) => {
-      console.log("success");
-    }
+  // Remove foodItem
+  $('.removeItemAjax').submit(function(e) {
+    e.preventDefault();
+    const postURL = $(this).attr("action");
+    const values = $(this).serialize();
+
+    $.ajax({
+      type: "POST",
+      url: postURL,
+      data: values,
+      success: (res) => {
+        console.log("success");
+      }
+    });
+  });
+
+  // ADD ITEM TO CART
+  $('.addToCartButton').on('click', function() {
+    $(this).closest('.addToCartForm').trigger("submit");
+    $(this).closest('.addToCartForm')[0].reset();
+  });
+
+  $('.addToCartForm').submit(function(e) {
+    e.preventDefault();
+    const postURL = $(this).attr("action");
+    const values = $(this).serialize();
+
+    $.ajax({
+      type: "POST",
+      url: postURL,
+      data: values,
+      success: (res) => {
+        console.log("success");
+      }
+    });
+
   });
 });
